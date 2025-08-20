52d897eccf07b4e8f2ac01430b479f5b35b4d689
mohamedredaelbana18 <mohamedredaelbana18@gmail.com>
2025-08-20 20:54:05 +0300

    Delete treasury directory

--- treasury/src/app/api/settlements/route.ts
+++
@@ -1,9 +0,0 @@
-import { NextRequest, NextResponse } from 'next/server';
-import { SettlementService } from '@/server/services/settlement/SettlementService';
-
-export async function POST(req: NextRequest) {
-  const { projectId, date, note, createdBy } = await req.json();
-  const res = await SettlementService.executeSettlement(projectId, { date: new Date(date), note, createdBy });
-  return NextResponse.json(res);
-}
-
